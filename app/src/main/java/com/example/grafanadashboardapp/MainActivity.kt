59116554a56f5ce3c28679a07e626baf6d3805b6
ReinerBalschun <reinermbalschun@gmail.com>
2024-09-17 14:08:30 +0200

    MainActivity.kt has Errors need to fix later!

--- app/src/main/java/com/example/grafanadashboardapp/MainActivity.kt
+++ app/src/main/java/com/example/grafanadashboardapp/MainActivity.kt
@@ -1,47 +1,43 @@
 package com.example.grafanadashboardapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.grafanadashboardapp.ui.theme.GrafanaDashboardAPPTheme
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.widget.Toast  
 
-class MainActivity : ComponentActivity() {
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit  
+    var webView: WebView  // Add semicolon here
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GrafanaDashboardAPPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Reiner",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)  
+
+
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(JavaScriptInterface(),  
+        "Android")
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("http://192.168.0.130:3000/public-dashboards/9c1f6f09da6a4769b18a75d681aa4b7d?orgId=1&refresh=auto")
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    inner class JavaScriptInterface {
+        @JavascriptInterface
+        fun showToast(message: String) {
+            // Zeigt einen Toast mit der übergebenen Nachricht an
+            Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GrafanaDashboardAPPTheme {
-        Greeting("Android")
+        @JavascriptInterface
+        fun openNewActivity() {
+            // Startet eine neue Activity
+            startActivity(Intent(this@MainActivity, OtherActivity::class.java))
+        }
     }
 }
