6a8d53e0f5c975ff7fea10ecb1da057c139d7b9d
ReinerBalschun <reinermbalschun@gmail.com>
2024-09-17 11:36:36 +0200

    Test #1 default settings

--- app/src/main/java/com/example/grafanadashboardapp/MainActivity.kt
+++ app/src/main/java/com/example/grafanadashboardapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GrafanaDashboardAPPTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Reiner",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
